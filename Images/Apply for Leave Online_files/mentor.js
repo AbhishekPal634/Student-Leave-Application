3010f00e1682ba9f92c22d4dc602b96ace4b5aad
Utkarsh Mandloi <utkarshmandloi12065@gmail.com>
2024-02-19 05:41:47 +0530

    Revert "Merge branch 'preview'"
    This reverts commit 7a82d837a5d6dadb09000572915cb71c68130d9b, reversing
    changes made to c0332f752aa6b448b661d5367bb296fc6a6ddb32.

--- Images/Apply for Leave Online_files/mentor.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const navBar = document.querySelector('.navbar');
-
-navBar.addEventListener('click', function (e) {
-	e.preventDefault();
-	navBar.Style.backgroundColor = 'black';
-});
